1f847d5fae0613cebf7796c7f06345253eed5f1d
Dhanesh AV <dhanesh012@gmail.com>
2022-07-07 14:11:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.exportVariable('DKAV', time +' some value ');
-  core.setSecret('DKAV');
+  core.exportVariable('DKAVSecret', 'TopSecret');
+  core.setSecret('TopSecret');
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
